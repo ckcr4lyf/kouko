e4b2b14191077db1fc33964daa93925c04944f35
Raghu Saxena <poiasdpoiasd@live.com>
2020-08-03 00:31:27 +0800

    next commit i swear

--- source/helpers/shuffle.ts
+++ source/helpers/shuffle.ts
@@ -2,7 +2,7 @@
  * Shuffles array in place. ES6 version
  * @param {Array} a items An array containing the items.
  */
-export const shuffle(a: any[]) => {
+export const shuffle = (a: any[]) => {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
