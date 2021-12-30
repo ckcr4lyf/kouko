3dcf9f8ac54601b2b19230a45ed75aa9fe322703
ckcr4lyf <poiasdpoiasd@live.com>
2021-12-31 01:03:31 +0800

    TCP Exporter

--- source/helpers/promExporter.ts
+++ source/helpers/promExporter.ts
@@ -1,5 +1,6 @@
 import redis from '../db/redis';
 import CONSTANTS from './constants';
+import { execSync } from 'child_process';
 
 /**
  * incrAccounce asynchronously calls an INCR operation
@@ -16,8 +17,26 @@ export const incrAnnounce = (reqDuration: number): void => {
  * on the redis key which keeps a count of total number
  * of bad announce requests (invalid params)    
  */
- export const incrBadAnnounce = (): void => {
+export const incrBadAnnounce = (): void => {
     void redis.incr(CONSTANTS.BAD_ANNOUNCE_COUNT_KEY);
+}
+
+export const getTcpData = (): string => {
+    const command = `netstat -tan | grep ':6969' | awk '{print $6}' | sort | uniq -c`;
+    
+    try {
+        const result = execSync(command).toString().split('\n').filter(el => el !== '').map(el => {
+            const trimmed = el.trim();
+            const parts = trimmed.split(' ');
+            console.log(parts);
+            return `kouko_tcp_stats{state="${parts[1]}"} ${parts[0]}\n`;
+        });
+
+        return result.join('');
+    } catch (e){
+        console.log(`Failed to get netstat data`);
+        return ``;
+    }
 }
 
 export const prepareExportData = async () => {
@@ -46,6 +65,7 @@ export const prepareExportData = async () => {
     exportData += `kouko_http_request_duration_sum{status_code="200", method="GET", path="announce"} ${avgRequestTime}\n`;
     exportData += `kouko_heap_total ${memoryUsage.heapTotal}\n`;
     exportData += `kouko_heap_used ${memoryUsage.heapUsed}\n`;
+    exportData += getTcpData();
 
     return exportData;
 }
